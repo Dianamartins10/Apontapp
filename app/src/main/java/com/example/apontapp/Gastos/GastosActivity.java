d50438eddcb085581f54edafadb8e01eb3c1b0d1
Daniel <sousa.daniel@sapo.pt>
2019-07-05 14:00:16 +0100

    Topbar with buttons (logout and gastos)

---
+++ app/src/main/java/com/example/apontapp/Gastos/GastosActivity.java
@@ -0,0 +1,13 @@
+package com.example.apontapp.Gastos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GastosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_gastos);
+    }
+}
